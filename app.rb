a07cada9d032aba28b25f8767b91135b53de2aa8
prince475 <danielmwai475@gmail.com>
2023-02-14 07:13:19 +0300

    Completed Running Ruby Code

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
+# pp [{ id: 1, hello: "World" },
+#     { id: 2, hello: "Ruby" },
+#     { id: 3, hello: "Moon" },
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
@@ -9,15 +9,41 @@
 # p [1,2,3]
 
 # def add
-    
+
 # end
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
+
+#app.rb
+# puts "Hello Ruby!"
+# puts "Hello Ruby!"
+# puts "Hello Ruby!"
+
+# print "Hello Ruby!"
+# print "Hello Ruby!"
+# print "Hello Ruby!"
+# print "Hello Ruby!"
+
+
+# p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!"
+
+print  "Pass this test, please."
+
+p [1, 2, 3]
+# string interpolation in ruby: use double quotes as so.
+
+# dog_name = 'Lucy'
+# puts "Say Hello to my dog #{dog_name}"
+
